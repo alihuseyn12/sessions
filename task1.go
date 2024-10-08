263bde8c916fcce9f8bf14887bd3d74a6effd6f3
alihuseyn12 <alihuseyn.999@gmail.com>
2024-10-08 23:03:43 +0400

    frist commit

---
+++ task1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 10
+	ptr := &x
+	fmt.Println("Value of x=", x)
+	fmt.Println("Address of x=", &ptr)
+	fmt.Println("Address of ptr=", *ptr)
+}
